4aeb9b76f0322d30cdc84e3c56e1dc1b1910921d
Luca Joshua <ljmosca@ciaopeople.com>
2023-01-31 14:54:15 +0100

    #0 - landing wip

---
+++ layouts/Default.vue
@@ -0,0 +1,44 @@
+<template>
+    <!-- <h2>Get ready to blast off into the gaming galaxy! </h2> -->
+    <main>
+        <slot />
+    </main>
+    <p>Get ready to blast off into the gaming galaxy!<br><br>
+        Starcade's website is currently under construction,
+        but sign up for our newsletter to stay informed about our next launch and receive exclusive intergalactic
+        goodies.
+    </p>
+    <newsletter-form @subscribed="log" headerText="Cool Newsletter" buttonText="Join in!" inputPlaceholder="Your email">
+        <template #description>
+            <p>My Newsletter Description</p>
+        </template>
+        <template #button-icon>
+            <!-- icon component -->
+        </template>
+    </newsletter-form>
+</template>
+
+<style lang="scss" scoped>
+h2 {
+    font-family: "Hogfish DEMO", sans-serif;
+    max-width: 300px;
+    line-height: 1.25;
+    margin: 0 auto;
+}
+
+p {
+    font-family: "Montserrat", sans-serif;
+    max-width: 500px;
+    font-weight: 300;
+}
+
+p,
+h2 {
+    text-align: center;
+    color: white;
+}
+</style>
+
+<script>
+
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+import LogoPlaceholder from '~~/components/LogoPlaceholder.vue';
+
+<template>
+    <LogoPlaceholder></LogoPlaceholder>
+</template>
+
+<style>
+body {
+    background: #2C4066;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    margin: 0;
+    padding: 0 2rem;
+}
+
+* {
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+}
+</style>
+
+<script>
+
+</script>
