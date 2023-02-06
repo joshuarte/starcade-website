77ee58a9aeb51989184641d6d1a5df61fe6b45d2
Luca Joshua <ljmosca@ciaopeople.com>
2023-02-06 15:28:44 +0100

    #0 - test rive anim

--- plugins/rive.client.js
+++ plugins/rive.client.js
@@ -1,7 +1,7 @@
 import { defineNuxtPlugin } from '#app' 
 export default defineNuxtPlugin((nuxtApp) => {
   new rive.Rive({
-    src: "https://www.starcade.it/rives/starcade-intro.riv",
+    src: "/rives/starcade-intro.riv",
     canvas: document.getElementById("canvas"),
     autoplay: true
   });
